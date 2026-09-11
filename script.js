451593875bd91ff79bb9e7f5d3426e56cffd7b62
juliettech1321 <adelekeazizatjuliet1@gmail.com>
2026-09-11 14:09:51 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const navLinks = document.querySelector(".nav-links");
+    navLinks.classList.toggle("active");
+}
+
+function showMessage() {
+    alert("Thanks for your interest in NexaTech! We will be in touch soon.");
+}
